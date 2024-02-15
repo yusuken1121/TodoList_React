1c724199f12ac2b049d910f94e64a392f7dc3f73
yusuken1121 <yusuken1121@gmail.com>
2024-02-14 22:59:46 -0800

    complete,delete,restore functions are done

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,74 @@ import './App.css';
 const App = () => {
   const [incompleteTodos, setincompleteTodos] = useState(["test1","test2"]);
   const [completedTodos, setcompletedTodos] = useState(["test3","test4"])
+  const[todoText, settodoText] = useState("")
+
+  const onChanageTodotext = (event) =>{ settodoText(event.target.value)}
+
+  const onClickAdd = () => {
+    const newTodos = [...incompleteTodos,todoText]
+    setincompleteTodos(newTodos)
+  }
+
+  const onClickDelete = (index) =>{
+    const newTodos = [...incompleteTodos]
+    newTodos.splice(index,1)
+    setincompleteTodos(newTodos)
+  }
+
+  const onClickComplete = (index) =>{
+    onClickDelete(index)
+    const newCompleteTodos = [...completedTodos,incompleteTodos[index]]
+    setcompletedTodos(newCompleteTodos)
+  }
+
+  const onClickRestore = (index) => {
+    const newCompleteTodos = [...completedTodos]
+    newCompleteTodos.splice(index,1)
+    setcompletedTodos(newCompleteTodos)
+
+    const newTodos = [...incompleteTodos,completedTodos[index]]
+    setincompleteTodos(newTodos)
+
+  }
+
   return (
     <>
       <div className='input-area'>
-        <input placeholder="Enter what you have to do" type="text" />
-        <button>Add</button>
+        <input placeholder="Enter what you have to do" type="text" value={todoText} onChange={onChanageTodotext}/>
+        <button onClick={onClickAdd}>Add</button>
       </div>
       <div className='incomplete-area'>
         <p className='title'>Incomplete tasks</p>
+
         <ul>
-          {incompleteTodos.map((todo)=>{
+          {incompleteTodos.map((todo,index)=>{
             return(
               <li key={todo}>
                 <div className='list-row'>
                   <p className='todo-item'>{todo}</p>
-                  <button>Complete</button>
-                  <button>Delete</button>
+                  <button onClick={() => onClickComplete(index)}>Complete</button>
+                  <button onClick={() => onClickDelete(index)}>Delete</button>
                 </div>
               </li>
             )
           })}
         </ul>
+
       </div>
       <div className='complete-area'>
         <p className='title'>Completed tasks</p>
         <ul>
-          {completedTodos.map((todo)=>{
+          {completedTodos.map((todo,index)=>{
             return(
               <li key={todo}>
                 <div className='list-row'>
                   <p className='todo-item'>{todo}</p>
-                  <button>Restore</button>
+                  <button onClick={()=>{onClickRestore(index)}}>Restore</button>
                 </div>
               </li>
             )
-          })}
-            
-            
+          })}  
         </ul>
       </div>
 
